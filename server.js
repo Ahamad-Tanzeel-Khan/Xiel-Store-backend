a74a979ed4088df951a57c004bd0e4ebef664b13
Ahamad-Tanzeel-Khan <117995820+Ahamad-Tanzeel-Khan@users.noreply.github.com>
2023-11-09 19:41:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.use(passport.session());
 
 app.use(
 	cors({
-		origin: "http://localhost:3000",
+		origin: "https://xiel-store.netlify.app",
 		methods: "GET,POST,PUT,DELETE",
 		credentials: true,
 	})
